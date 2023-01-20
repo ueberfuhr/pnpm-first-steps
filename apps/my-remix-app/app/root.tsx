32bc6e06f91b27f9fd4465add7c188e76226c351
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-01-20 11:03:00 +0100

    Add app and package

---
+++ apps/my-remix-app/app/root.tsx
@@ -0,0 +1,29 @@
+import type { MetaFunction } from "@remix-run/node";
+import {
+  LiveReload,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <title>My Remix App</title>
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# PNPM sample
+
+## Use the project
+
+```bash
+# Run script on single project
+# ❯ pnpm --filter <package-name> <script>
+pnpm --filter my-remix-app build
+pnpm --filter shared-ui build
+# Add dependency to single project
+# ❯ pnpm add --filter <package-name> <dependency> <options>
+pnpm add --filter shared-ui typescript -D
+# Add dependency between app and package
+pnpm add shared-ui --filter my-remix-app --workspace
+```
